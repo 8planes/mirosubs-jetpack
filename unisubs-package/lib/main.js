a0708d438a1be7bd22da0b4ce33606c5f62c2558
Adam Duston <aduston@gmail.com>
2010-09-02 22:35:41 -0400

    This finishes the jetpack extension, version 0.01

--- unisubs-package/lib/main.js
+++ unisubs-package/lib/main.js
@@ -1,1 +1,36 @@
-# see https://jetpack.mozillalabs.com/tutorial.html
+const widgets = require("widget");
+const tabs = require("tabs");
+
+function addWidgetizer(win, doc) {
+    if (!win.jetpackMiroWidgetized) {
+        win.jetpackMiroWidgetized = true;
+        var head = doc.getElementsByTagName('head')[0];
+        var widgetScript = doc.createElement('script');
+        widgetScript.type = 'text/javascript';
+        widgetScript.src = 'http://dev.universalsubtitles.org/site_media/js/mirosubs-widgetizer.js';
+        widgetScript.charset = 'UTF-8';
+        head.appendChild(widgetScript);
+    }
+}
+
+widgets.add(widgets.Widget({
+    label: "Find or create subtitles for videos in this page",
+    image: "http://s3.www.universalsubtitles.org/images/favicon.ico",
+    onClick: function(e) {
+        addWidgetizer(
+            tabs.activeTab.contentWindow,
+            tabs.activeTab.contentDocument);
+    }
+}));
+
+/******
+  Notes for the near future
+
+  use tabs.onReady, similar to "automatic boom" in 
+  https://jetpack.mozillalabs.com/tutorial.html
+
+  We can use this to give user the option to add widgetizer to 
+  any page they navigate to.
+
+
+ */
